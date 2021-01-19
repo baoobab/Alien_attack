8fed71cf427f045b35afd8ec9adc53c8833ccd4e
baobab <v.xxl.90@mail.ru>
2021-01-19 20:44:42 +0400

    +босс и рандомные уровни (ВАДИМ)

--- Game_code.py
+++ Game_code.py
@@ -1,6 +1,6 @@
 import os
 import sys
-
+import random
 import pygame
 
 pygame.font.init()
@@ -15,14 +15,16 @@ y = 550
 
 ship_height = 90
 ship_width = 80
+
 bullets = []
 
-aliens_lvl1 = []
-aliens_lvl2 = []
-x_alien_lvl1 = x_alien_lvl2 = 30
-y_alien_lvl1 = y_alien_lvl2 = 30
-alien_speed_lvl1 = 0.4
-alien_speed_lvl2 = 1.2
+aliens = []
+x_alien = 30
+y_alien = 30
+alien_speed = 0
+lvl = 0
+score = 0
+best_score = 0
 
 
 class bulleti():
@@ -39,13 +41,27 @@ class bulleti():
         pygame.draw.circle(self.win, self.color, (self.x, self.y), self.r)
 
 
-def draw_ammo(screen):
+def draw_txt(screen):
     font = pygame.font.Font(None, 100)
     font2 = pygame.font.Font(None, 25)
     text = font.render(f"{5 - len(bullets)}", True, (255, 0, 0))
     text2 = font2.render("Патроны", True, (255, 0, 0))
+    text3 = font2.render(f"Уровень: {lvl}", True, (255, 0, 0))
     screen.blit(text, (500, 15))
     screen.blit(text2, (470, 5))
+    screen.blit(text3, (420, 610))
+
+
+def draw_score(screen, flag):
+    if flag == 1:
+        font = pygame.font.Font(None, 25)
+        text = font.render(f"Счёт: {score}", True, (255, 0, 0))
+        screen.blit(text, (5, 610))
+    else:
+        count_score(1)
+        font = pygame.font.Font(None, 50)
+        text2 = font.render(f"{best_score}", True, (255, 255, 255))
+        screen.blit(text2, (330, 600))
 
 
 def load_image(name, colorkey=None):
@@ -64,6 +80,17 @@ def load_image(name, colorkey=None):
     else:
         image = image.convert_alpha()
     return image
+
+
+def count_score(flag):
+    global best_score
+    if flag == 1:
+        with open("settings.txt", mode="r") as f:
+            data = f.read()
+            best_score = int(data)
+    else:
+        with open("settings.txt", mode="w") as f:
+            f.write(str(best_score))
 
 
 def is_cross(a, b):
@@ -85,15 +112,61 @@ def terminate():
     sys.exit()
 
 
+def lvl_generate():
+    global alien_speed, x_alien, y_alien, lvl
+    x_alien = 30
+    y_alien = 30
+    aliens.clear()
+    lvl += 1
+
+    if lvl % 5 != 0:
+        if random.randrange(0, 2) == 1:
+            for i in range(6):
+                _ = []
+                for j in range(3):
+                    if lvl < 5:
+                        _.append(random.choice([0, 2]))
+                    else:
+                        _.append(2)
+                aliens.append(_)
+            alien_speed = alien_speed // 2
+
+        else:
+            for i in range(6):
+                _ = []
+                for j in range(3):
+                    if lvl < 5:
+                        _.append(random.choice([0, 3]))
+                    else:
+                        _.append(3)
+                aliens.append(_)
+    else:
+        for i in range(6):
+            _ = []
+            for j in range(3):
+                if i == 3 and j == 2:
+                    _.append(10)
+                else:
+                    _.append(0)
+            aliens.append(_)
+        alien_speed = alien_speed // 2
+
+    alien_speed += 0.05
+
+
 def start_menu():
     pygame.mouse.set_visible(True)
     screen.blit(menu_bg, (0, 0))
     screen.blit(menu_btn1, (120, 270))
     screen.blit(menu_btn2, (120, 390))
+    screen.blit(menu_bestscore, (120, 580))
+    draw_score(screen, 0)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 terminate()
+
             elif event.type == pygame.MOUSEMOTION:
                 if 420 > pygame.mouse.get_pos()[0] > 120 and 370 > pygame.mouse.get_pos()[1] > 270:
                     screen.blit(menu_btn1a, (120, 270))
@@ -118,101 +191,80 @@ def start_menu():
 
 background = load_image("bg.jpg")
 ship = load_image("spaceship.png")
-alien = load_image("alien.jpg", -1)
-game_over = load_image("gameover.jpg", -1)
+game_over = load_image("gameover.png", -1)
 name = load_image("name.png", -1)
-alien2 = load_image("alien2.jpg", -1)
-alien3 = load_image("alien3.jpg", -1)
+
 menu_bg = load_image("menu_bg3.png")
 menu_bg = pygame.transform.scale(menu_bg, (550, 650))
-menu_btn1 = load_image("btn_1t.png")  # "t" означает что это тестовая кнопка
-menu_btn1a = load_image("btn_1ta.png")  # "a" означает что это активная кнопка (меняется на неё при наведении)
+menu_btn1 = load_image("btn_1t.png")
+menu_btn1a = load_image("btn_1ta.png")
 menu_btn2 = load_image("btn_2t.png")
 menu_btn2a = load_image("btn_2ta.png")
+menu_bestscore = load_image("bestscore.png")
+menu_bestscore = pygame.transform.scale(menu_bestscore, (200, 70))
 
-alien = pygame.transform.scale(alien, (50, 50))
+alien4 = load_image("boss.png", -1)
+alien1 = load_image("alien2.png", -1)
+alien2 = load_image("alien.png", -1)
+alien3 = load_image("alien3.png", -1)
+alien1 = pygame.transform.scale(alien1, (50, 50))
 alien2 = pygame.transform.scale(alien2, (50, 50))
 alien3 = pygame.transform.scale(alien3, (50, 50))
+alien4 = pygame.transform.scale(alien4, (150, 100))
+aliens_img = {1: alien1, 2: alien2, 3: alien3, 10: alien4}
 
-lvl = 1
-
-for i in range(6):
-    _ = []
-    for j in range(3):
-        _.append(2)
-    aliens_lvl1.append(_)
-
-for i in range(6):
-    _ = []
-    for j in range(2):
-        _.append(1)
-    aliens_lvl2.append(_)
-# print(aliens)
-
+lvl_generate()
 start_menu()
+count_score(1)
+
 running = True
 while running:
     screen.blit(background, (0, 0))
     screen.blit(name, (125, 0))
     screen.blit(ship, (x, y))
 
-    if lvl == 1:
-        for i in range(6):
-            for j in range(3):
-                if aliens_lvl1[i][j] == 2:
-                    screen.blit(alien, (x_alien_lvl1 * 3 * i + 25, y_alien_lvl1 + j * 50))
+    if score > best_score:
+        best_score = score
+
+    for i in range(6):
+        for j in range(3):
+            if aliens[i][j] > 0:
+                if lvl % 5 != 0:
+                    screen.blit(aliens_img[aliens[i][j]], (x_alien * 3 * i + 25, y_alien + j * 50))
                     for bullet in bullets:
-                        if (x_alien_lvl1 * 3 * i + 25 + 50 > bullet.x > x_alien_lvl1 * 3 * i + 25) \
-                                and y_alien_lvl1 + j * 50 + 50 > bullet.y > y_alien_lvl1 + j * 50:
-                            if aliens_lvl1[i][j] == 1:
-                                aliens_lvl1[i][j] = 0
+                        if (x_alien * 3 * i + 25 + 50 > bullet.x > x_alien * 3 * i + 25) \
+                                and y_alien + j * 50 + 50 > bullet.y > y_alien + j * 50:
+                            if aliens[i][j] == 3:
+                                score += 1
+                                aliens[i][j] = 0
+                            if 3 > aliens[i][j] > 0:
+                                    aliens[i][j] -= 1
                             else:
-                                aliens_lvl1[i][j] = 1
+                                score += 1
+                            bullets.remove(bullet)
+                else:
+                    screen.blit(aliens_img[10], (x_alien * 3 * i + 25, y_alien + j * 50))
+                    for bullet in bullets:
+                        if (x_alien * 3 * i + 25 + 150 > bullet.x > x_alien * 3 * i + 25) \
+                                and y_alien + j * 50 + 100 > bullet.y > y_alien + j * 50:
+                            if aliens[i][j] > 0:
+                                aliens[i][j] -= 1
+                            else:
+                                aliens[i][j] = 0
                             bullets.remove(bullet)
 
-                elif aliens_lvl1[i][j] == 1:
-                    screen.blit(alien2, (x_alien_lvl1 * 3 * i + 25, y_alien_lvl1 + j * 50))
-                    for bullet in bullets:
-                        if (x_alien_lvl1 * 3 * i + 25 + 50 > bullet.x > x_alien_lvl1 * 3 * i + 25) \
-                                and y_alien_lvl1 + j * 50 + 50 > bullet.y > y_alien_lvl1 + j * 50:
-                            if aliens_lvl1[i][j] == 1:
-                                aliens_lvl1[i][j] = 0
-                            else:
-                                aliens_lvl1[i][j] = 1
-                            bullets.remove(bullet)
-
-                    # ф-я для отслеживания столкновений
                 if is_cross([x, y, x + ship_width, y + ship_height],
-                            [x_alien_lvl1 * 3 * i + 25 + 50, y_alien_lvl1 + j * 50 + 50, x_alien_lvl1 * 3 * i + 25, y_alien_lvl1 + j * 50]):
+                            [x_alien * 3 * i + 25 + 50, y_alien + j * 50 + 50, x_alien * 3 * i + 25,
+                             y_alien + j * 50]):
                     # если врезались во врага, то игра окончена
                     screen.blit(game_over, (130, 250))
-                    # running = False
+                    count_score(0)
 
-        y_alien_lvl1 += alien_speed_lvl1
-        if aliens_lvl1 == [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]:
-            lvl += 1
-    if lvl == 2:
-        for i in range(6):
-            for j in range(2):
-                if aliens_lvl2[i][j] == 1:
-                    screen.blit(alien3, (x_alien_lvl2 * 3 * i + 25, y_alien_lvl2 + j * 50))
-                    for bullet in bullets:
-                        if (x_alien_lvl2 * 3 * i + 25 + 50 > bullet.x > x_alien_lvl2 * 3 * i + 25) \
-                                and y_alien_lvl2 + j * 50 + 50 > bullet.y > y_alien_lvl2 + j * 50:
-                            if aliens_lvl2[i][j] == 1:
-                                aliens_lvl2[i][j] = 0
-                            else:
-                                aliens_lvl2[i][j] = 1
-                            bullets.remove(bullet)
+        y_alien += alien_speed
 
-                    # ф-я для отслеживания столкновений
-                if is_cross([x, y, x + ship_width, y + ship_height],
-                            [x_alien_lvl2 * 3 * i + 25 + 50, y_alien_lvl2 + j * 50 + 50, x_alien_lvl2 * 3 * i + 25, y_alien_lvl2 + j * 50]):
-                    # если врезались во врага, то игра окончена
-                    screen.blit(game_over, (130, 250))
-                    # running = False
+        if aliens == [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]:
+            lvl_generate()
 
-        y_alien_lvl2 += alien_speed_lvl2
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -234,7 +286,9 @@ while running:
     for bullet in bullets:
         bullet.draw(screen)
 
-    draw_ammo(screen)
+    draw_score(screen, 1)
+    draw_txt(screen)
+
     x = pygame.mouse.get_pos()[0]
     y = pygame.mouse.get_pos()[1]
 
